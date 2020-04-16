84fb656eb089eb23c585293dddcc702bf0e3b559
leighd2008 <leighd2008@gmail.com>
2020-04-16 08:33:36 -0400

    created firebase database to store user information

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from "./components/header/header";
 import Spinner from "./components/spinner/spinner";
 import ErrorBoundary from "./components/error-boundary/error-boundary";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -32,9 +32,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
