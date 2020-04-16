c7503feb022b1608ab83fa00e72643f23657b110
leighd2008 <leighd2008@gmail.com>
2020-04-16 14:41:16 -0400

    updating to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -75,11 +75,11 @@ class App extends React.Component {
                 <Route
                   exact
                   path="/Sign_In"
-                  render={(props) =>
+                  render={() =>
                     this.props.currentUser ? (
                       <Redirect to="/" />
                     ) : (
-                      <SignInAndSignUpPage {...props} />
+                      <SignInAndSignUpPage />
                     )
                   }
                 />
@@ -92,7 +92,11 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
