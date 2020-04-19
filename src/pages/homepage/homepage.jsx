61084d92616ec35b22caceb9657769feff6507ab
leighd2008 <leighd2008@gmail.com>
2020-04-19 13:52:07 -0400

    tweaking the layout

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -19,7 +19,7 @@ const HomePage = () => {
           fc={({ theme }) => theme.palletPurple}
           text={
              <div>
-              <h3>Hi, </h3>
+              <h3>Welcome to You and Me Web Creations </h3>
               <span>My name is Diane Leigh, I am a freelance web developer/designer, located in Leavittsburg Ohio. After 30+ years in medical research I needed freedom and flexibility to enjoy my granddaugter. So, let me help you get your presence known to the world. For more background check out my</span>
             </div>
             }
@@ -37,7 +37,11 @@ const HomePage = () => {
         <ColoredDiv
           bg={({ theme }) => theme.palletOrange}
           fc={({ theme }) => theme.palletBlue}
-          text="We can create everything from simple single page websites to multipage progressive web apps using cloud storage, complex forms for retrieving data from end-users and checkout pages for ecommerce. We can integrate your calendar and incorporate a schedualing component."
+          text={
+            <div>
+            "We can create everything from simple single page websites to multipage progressive web apps using cloud storage, complex forms for retrieving data from end-users and checkout pages for ecommerce. We can integrate your calendar and incorporate a schedualing component."
+            </div>
+            }
           go_to={
             <a href="Portfolio" rel="noopener noreferrer">
               View my work here!
@@ -48,7 +52,7 @@ const HomePage = () => {
         <ColoredDiv
           bg={({ theme }) => theme.palletBlue}
           fc={({ theme }) => theme.palletGreen}
-          text="My favorite part of web design is figuring out how to put the pieces together in unique and refreshing ways. We can work from your vision, or mine to create the website of your dreams. So let's get you known to the world"
+          text="My favorite part of web design is figuring out how to put the pieces together in unique and refreshing ways. We can work from your vision, or mine to create the website of your dreams. So let's get you known to the world!"
           rotation="rotate(-5deg)"
         ></ColoredDiv>
         <ColoredDiv
